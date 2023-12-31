01e6f4b9dbe9973ea560877ffe4487bd1def7e00
yosef mesfin <yosefmesfin89@gmail.com>
2023-12-31 13:11:59 +0300

    first commit

---
+++ graphbase/grafbase.config.ts
@@ -0,0 +1,31 @@
+import { graph, config, auth } from "@grafbase/sdk";
+
+const g = graph.Standalone();
+// @ts-ignore
+const User = Graph.model("User", {
+	name: g.string().length({ min: 2, max: 100 }),
+	email: g.string().unique(),
+	avatarUrl: g.url(),
+	description: g.string().length({ min: 2, max: 1000 }).optional(),
+	githubUrl: g.url().optional(),
+	linkedinUrl: g.url().optional(),
+	projects: g
+		.relation(() => Project)
+		.list()
+		.optional(),
+});
+
+// @ts-ignore
+const Project = Graph.model("Project", {
+	title: g.string().length({ min: 3 }),
+	description: g.string(),
+	image: g.url(),
+	liveSiteUrl: g.url(),
+	githubUrl: g.url(),
+	category: g.string().search(),
+	createdBy: g.relation(() => User),
+});
+
+export default config({
+	graph: g,
+});
